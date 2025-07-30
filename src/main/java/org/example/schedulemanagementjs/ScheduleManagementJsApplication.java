e1fd91e3d1c2fd282f0e75160f3eb35cb060ffc3
hanjsnote <enlun@naver.com>
2025-07-30 22:19:56 +0900

    일정관리앱 커밋

---
+++ src/main/java/org/example/schedulemanagementjs/ScheduleManagementJsApplication.java
@@ -0,0 +1,13 @@
+package org.example.schedulemanagementjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementJsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementJsApplication.class, args);
+    }
+
+}
